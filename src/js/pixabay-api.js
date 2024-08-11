7ef61b89759fab4676b3e332fa2d7c937cd22fce
alina785 <alinochkaa.bilogubka@gmail.com>
2024-08-11 16:49:53 +0200

    all changes

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+const refs = {
+imageSearchForm: document.querySelector('.search-form'),
+imageSearchInput: document.querySelector('.search-input'),
+submitButton: document.querySelector('.search-btn'),
+imageList: document.querySelector('.images-list'),
+}
+
+export function getImages(request) {
+    const BASE_URL = 'https://pixabay.com';
+    const END_POINT = '/api/';
+    const PARAMS = new URLSearchParams({
+        key: '44710644-2154e124280f212c40f530ba7',
+        q: request,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: 1,
+        per_page: 30,
+    });
+
+    const url = `${BASE_URL}${END_POINT}?${PARAMS}`;
+    console.log(url);
+
+    const options = {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content - Type': 'application/json',
+
+            'X-RateLimit-Limit': '100',
+            'X-RateLimit-Remaining': '99',
+            'X-RateLimit-Reset': '0.6',
+    },
+}
+
+    return fetch(url)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else {
+                throw new Error(res.status);
+                      }
+        });
+}
